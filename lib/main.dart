7501d0074c131687b1548d78982b9f491b57c3a1
admin <rathod.kishanApple@gmail.com>
2022-09-10 13:30:22 +0530

    COMMIT ON Manan Branch23

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        //Kishan Branch
+        //Manan Branch121
 
         primarySwatch: Colors.blue,
       ),
