a3eb890ba694b945154179017120595ae7b45fc2
Manan Jain <jainimans@gmail.com>
2022-09-10 12:41:22 +0530

    Commit 10_SEP_2022 From Manan Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         //CodeEdited From Manan Update
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
